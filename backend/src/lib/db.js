0435ee1a0e9272965274af416282db770b82c0d1
Rohit Shukla <rohitshukla7746@gmail.com>
2026-03-08 22:37:02 +0530

    Update DB connection for Supabase Session Pooler

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,11 +5,12 @@ const { Pool } = pkg;
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT),
+  // database: process.env.DB_NAME,
+  // user: process.env.DB_USER,
+  // password: String(process.env.DB_PASSWORD)
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   },
