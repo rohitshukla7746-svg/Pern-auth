34e232010db21a40993dbd92289a4438fabc8a09
Rohit Shukla <rohitshukla7746@gmail.com>
2026-03-09 14:34:11 +0530

    update env variable usage

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,3 +1,47 @@
+// // import pkg from "pg";
+// // import dotenv from "dotenv";
+// // const { Pool } = pkg;
+
+// // dotenv.config();
+
+// // const pool = new Pool({
+// //   host: process.env.DB_HOST,
+// //   port: Number(process.env.DB_PORT),
+// //   database: process.env.DB_NAME,
+// //   user: process.env.DB_USER,
+// //   password: String(process.env.DB_PASSWORD),
+// //   ssl: {
+// //     rejectUnauthorized: false
+// //   },
+// // });
+
+// // export default pool;
+
+// import pkg from "pg";
+// import dotenv from "dotenv";
+// const { Pool } = pkg;
+
+// dotenv.config();
+
+// const pool = new Pool({
+//   // host: process.env.DB_HOST,
+//   // port: Number(process.env.DB_PORT),
+//   // database: process.env.DB_NAME,
+//   // user: process.env.DB_USER,
+//   // password: String(process.env.DB_PASSWORD)
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   },
+// });
+
+// pool.connect()
+//   .then(() => console.log("✅ DB connected successfully"))
+//   .catch((err) => console.error("❌ DB not connected", err));
+
+// export default pool;
+
+
 import pkg from "pg";
 import dotenv from "dotenv";
 const { Pool } = pkg;
@@ -5,19 +49,15 @@ const { Pool } = pkg;
 dotenv.config();
 
 const pool = new Pool({
-  // host: process.env.DB_HOST,
-  // port: Number(process.env.DB_PORT),
-  // database: process.env.DB_NAME,
-  // user: process.env.DB_USER,
-  // password: String(process.env.DB_PASSWORD)
-  connectionString: process.env.DATABASE_URL,
+  // Host corrected to aws-1 as per your Supabase screenshot
+  user: 'postgres.zeytmkdagscytedatotk',
+  host: 'aws-1-ap-south-1.pooler.supabase.com',
+  database: 'postgres',
+  password: 'jjhh-kkhh-7746',
+  port: 5432, 
   ssl: {
     rejectUnauthorized: false
-  },
+  }
 });
-
-pool.connect()
-  .then(() => console.log("✅ DB connected successfully"))
-  .catch((err) => console.error("❌ DB not connected", err));
 
 export default pool;
